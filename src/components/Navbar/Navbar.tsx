54d0f377bd15262774a2bc68d02f33f36d27d372
Leonardo Cueto <leocueto1999@gmail.com>
2025-03-25 23:27:00 -0300

    Add Navbar component for navigation between Home and Favorites

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,27 @@
+import { usePage } from '@/context/context'
+import './style.css'
+export const Navbar = () => {
+    const { currentPage, setCurrentPage } = usePage()
+    return (
+        <nav className='navbar'>
+            <a
+                className={currentPage === '/' ? 'active' : ''}
+                onClick={e => {
+                    e.preventDefault()
+                    setCurrentPage('/')
+                }}
+            >
+                Home
+            </a>
+            <a
+                className={currentPage === '/favorites' ? 'active' : ''}
+                onClick={e => {
+                    e.preventDefault()
+                    setCurrentPage('/favorites')
+                }}
+            >
+                Favorites
+            </a>
+        </nav>
+    )
+}
