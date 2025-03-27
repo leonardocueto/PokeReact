51c8eda55157e0dbb2c2e00f10b76f21ab90eb37
Leonardo Cueto <leocueto1999@gmail.com>
2025-03-26 21:46:05 -0300

    Add favorite functionality to Card component with Heart icon

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,16 +1,42 @@
 import { useCallback, useState } from 'react'
 import './style.css'
 import { PokemonDetails, Ability } from '@/models'
+import { Heart } from '@/components'
+import { useFavorites } from '@/context'
 
 interface CardProps {
     data: PokemonDetails
-    loading: boolean
+    loading?: boolean
 }
 
 export const Card = ({ data, loading }: CardProps) => {
     const [active, setActive] = useState(false)
+    const [isFavorite, setIsFavorite] = useState(false)
+
+    const { addFavorites, removeFavorites } = useFavorites()
 
     const toggleActive = useCallback(() => setActive(prev => !prev), [])
+    const getTypeClass = () => {
+        const typeName = data.types?.[0]?.type?.name
+        if (!typeName) return ''
+
+        return `bg-${typeName.toLowerCase()}`
+    }
+
+    const typeUpperLetter =
+        data.types[0].type.name.charAt(0).toUpperCase() +
+        data.types[0].type.name.slice(1)
+
+    const handleHeartClick = (e: React.MouseEvent) => {
+        e.stopPropagation()
+
+        if (!isFavorite) {
+            addFavorites(data)
+        } else {
+            removeFavorites(data.id)
+        }
+        setIsFavorite(prev => !prev)
+    }
 
     return (
         <>
@@ -18,35 +44,95 @@ export const Card = ({ data, loading }: CardProps) => {
                 <div className='cards loading'>loading</div>
             ) : (
                 <div
-                    className={`cards ${active ? 'cards-active' : ''} ${
-                        data.types?.[0]?.type?.name
-                            ? `bg-${data.types[0].type.name}`
-                            : ''
-                    }`}
+                    className={`cards ${
+                        active ? 'cards-active' : ''
+                    } ${getTypeClass()}`}
                     onClick={toggleActive}
                 >
-                    <div className='card-front'>
-                        <h1>{data.name}</h1>
-                        <img
-                            loading='lazy'
-                            src={data.sprites.front_default}
-                            alt={`img ${data.name}`}
-                        />
-                    </div>
+                    <main className='card-front'>
+                        <div
+                            className='favorite-heart'
+                            onClick={handleHeartClick}
+                        >
+                            <Heart filled={isFavorite} />
+                        </div>
+                        <header className='card-header'>
+                            <div className='card-header-title'>
+                                <h1 className='card-title'>{data.name}</h1>
+                                <p>{typeUpperLetter}</p>
+                            </div>
+                            <div className='bg-hp'>
+                                HP {data.stats[0].base_stat}
+                            </div>
+                        </header>
+                        <section className='card-img'>
+                            <div className='card-bg-img'>
+                                <img
+                                    loading='lazy'
+                                    src={data.sprites.front_default}
+                                    alt={`img ${data.name}`}
+                                />
+                            </div>
+                        </section>
+                        <section className='card-stats'>
+                            <div>
+                                <div className='card-attack'>
+                                    <div>Attack</div>
+                                    <div>{data.stats[1].base_stat}</div>
+                                </div>
+                                <div className='card-attack-percent'>
+                                    <div
+                                        className='card-attack-percentage'
+                                        style={{
+                                            width: `${
+                                                (data.stats[1].base_stat /
+                                                    150) *
+                                                100
+                                            }%`
+                                        }}
+                                    />
+                                </div>
+                            </div>
+                            <div>
+                                <div className='card-attack'>
+                                    <div>Defense</div>
+                                    <div>{data.stats[2].base_stat}</div>
+                                </div>
+                                <div className='card-attack-percent'>
+                                    <div
+                                        className='card-attack-percentage'
+                                        style={{
+                                            width: `${
+                                                (data.stats[2].base_stat /
+                                                    150) *
+                                                100
+                                            }%`
+                                        }}
+                                    />
+                                </div>
+                            </div>
+                        </section>
+                    </main>
 
-                    <div className='card-back'>
-                        <h2>Información</h2>
-                        <p>Altura: {data.height} Cm</p>
-                        <p>Peso: {data.weight} Kg</p>
-                        <p>Habilidades:</p>
-                        <ul>
-                            {data.abilities.map(
-                                (ability: Ability, index: number) => (
-                                    <li key={index}>{ability.ability.name}</li>
-                                )
-                            )}
-                        </ul>
-                    </div>
+                    <main className='card-back'>
+                        <h1 className='card-subtitle'>Information</h1>
+                        <div className='card-description'>
+                            <div>Heigth: {data.height} Cm</div>
+                            <div>Weigth: {data.weight} Kg</div>
+                            <div className='card-abilities'>
+                                <div>Abilities:</div>
+                                <ul className='abilities-list'>
+                                    {data.abilities.map(
+                                        (ability: Ability, index: number) => (
+                                            <li key={index}>
+                                                {ability.ability.name}
+                                            </li>
+                                        )
+                                    )}
+                                </ul>
+                            </div>
+                        </div>
+                    </main>
                 </div>
             )}
         </>
