e5e5f5f8bb77199a3d497f3eab4ae196cd1f962c
Leonardo Cueto <leocueto1999@gmail.com>
2025-03-26 21:45:53 -0300

    Add Heart component for displaying a customizable heart icon

---
+++ src/components/Heart.tsx
@@ -0,0 +1,20 @@
+export const Heart = ({ filled = false }: { filled?: boolean }) => (
+    <svg
+        xmlns='http://www.w3.org/2000/svg'
+        width='24'
+        height='24'
+        viewBox='0 0 24 24'
+        fill={filled ? 'red' : 'none'}
+        stroke='currentColor'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+    >
+        <path
+            stroke='none'
+            d='M0 0h24v24H0z'
+            fill='none'
+        />
+        <path d='M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572' />
+    </svg>
+)
